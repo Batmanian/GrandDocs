0b3aa277aa86fb39d294f443526ff8e2f0d19c86
Jack Dunstan <jack@dunsta.net>
2020-02-02 17:56:43 +1100

    Fixed asset links

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -21,8 +21,8 @@ const users = [{
 const siteConfig = {
     title: 'Test Site', // Title for your website.
     tagline: 'A website for testing',
-    url: 'https://your-docusaurus-test-site.com', // Your website URL
-    baseUrl: '/', // Base URL for your project */
+    url: 'https://batmanian.github.io', // Your website URL
+    baseUrl: '/GrandDocs/', // Base URL for your project */
     // For github.io type URLs, you would set the url and baseUrl like:
     //   url: 'https://facebook.github.io',
     //   baseUrl: '/test-site/',
