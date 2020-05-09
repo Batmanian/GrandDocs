4d5ca2e098c37e828d6ca184d0d85f39c1a62572
Jack Dunstan <jack@dunsta.net>
2020-05-09 21:53:04 +1000

    Switch to Hugo from Docusaurus

--- sidebars.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    someSidebar: {
-        Rules: ['definitions', 'rules', 'ownersobligations', 'alterations', 'vehicles', 'insurance', 'security', 'waste', 'notice', 'contractors', 'notification', 'ownerscorporationrules', 'retail', 'provision', 'storage', 'fire', 'ownerscorporationmanager', 'notices', 'complaints', 'gst', 'heritage', 'hotel', 'relocations', ],
-        Manual: ['manual'],
-        //Dev: ['mdx', 'doc1', 'doc2', 'doc3'],
-    },
-};
